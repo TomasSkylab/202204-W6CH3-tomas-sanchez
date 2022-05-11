83c529ca63752a50565b07a8114c95cc1eb3ace8
Tomas Sanchez <tms90@outlook.es>
2022-05-11 20:37:59 +0200

    Added store file with provider root

---
+++ src/redux/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import phoneReducer from "../features/phoneSlice";
+
+const store = configureStore({
+  reducer: {
+    phone: phoneReducer,
+  },
+});
+
+export default store;
